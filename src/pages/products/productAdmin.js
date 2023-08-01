85408d859bdd125eef6fda8938fab94c45cf9578
rfnasution01 <rivaifnasution@gmail.com>
2023-08-01 11:04:12 +0700

    clean code

--- src/pages/products/productAdmin.js
+++ src/pages/products/productAdmin.js
@@ -388,6 +388,7 @@ const ProductAdmin = () => {
             >
                 <img
                     src={ImgTemplate}
+                    alt="img template"
                 />
                 <Typography
                     sx={{
